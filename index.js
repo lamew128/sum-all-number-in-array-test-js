380ad8f0110215e8636017c8a17392a2223e652f
Eugene <lamew128@gmail.com>
2022-04-08 14:22:21 -0400

    sum array

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+  for (let i of array) {
+    if (Array.isArray(i)) {
+      sum += sumItems(i);
+    } else {
+      sum += i;
+    }
+  }
+  return sum;
 }
 
 module.exports = sumItems;
